e326ab48f573f1b17763a1636d88a1b55fb779e7
emilio <jemilio9606@gmail.com>
2023-08-20 23:52:23 -0400

    feat: service discovery

--- items/src/main/java/com/poc/microservice/items/ItemsApplication.java
+++ items/src/main/java/com/poc/microservice/items/ItemsApplication.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
@@ -20,6 +21,7 @@ import java.util.Optional;
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @RequiredArgsConstructor
+@EnableDiscoveryClient
 public class ItemsApplication implements CommandLineRunner {
     private final ItemRepository itemRepository;
 
